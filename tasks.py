14b064a7148e973938533bf95e81898f58729db5
RonaldGalea <raulronaldgalea@gmail.com>
2023-09-05 12:53:49 +0200

    add some dependencies just for show

--- tasks.py
+++ tasks.py
@@ -1,5 +1,8 @@
 # tasks.py
 from celery import Celery
+import numpy as np
+import pandas as pd
+import scipy
 
 # Creating a Celery instance
 # 'redis://localhost:6379/0' points to a local Redis instance
@@ -8,4 +11,6 @@ app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379
 @app.task
 def my_dummy_task(arg1: int, arg2: str) -> str:
     print(f" Running dummy task with arguments: {arg1}, {arg2}")
+    print("Heavy work being done...")
+    # numpy, pandas, scipy stuff
     return f"Task completed with arguments: {arg1}, {arg2}"
